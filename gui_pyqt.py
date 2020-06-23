738411502408044c532e421e377988672a7d252a
TimotheosS <62542566+TimotheosS@users.noreply.github.com>
2020-06-23 14:47:58 +0200

    update Boxes and Yolo-Videos

--- gui_pyqt.py
+++ gui_pyqt.py
@@ -4,13 +4,13 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QTableWidgetItem, QFileDialog,QWidget, QDesktopWidget
 from PyQt5.QtGui import QPixmap
 import cv2
-import os
 import numpy as np
+import os
 
-## Change the Directory Paths
 configPath = "yolov3.cfg"
 weightsPath = "yolov3.weights"
 classesPath = "yolov3.txt"
+
 
 class App(QWidget):
     
@@ -31,6 +31,8 @@ class App(QWidget):
             
         self.COLORS = np.random.uniform(0, 255, size=(len(self.classes), 3))        
         self.net = cv2.dnn.readNet(weightsPath,configPath)
+        self.ln = self.net.getLayerNames()
+        self.ln = [self.ln[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
         
         self.savedImages= []
         self.seen = set()
@@ -38,11 +40,25 @@ class App(QWidget):
         self.label = form.loadedImgLbl
         self.quitBut = form.qbtn.clicked.connect(self.quitApp)
         self.loadImButton = form.loadIm.clicked.connect(self.loadImageFunction)
+        self.loadVidButton = form.loadVid.clicked.connect(self.loadVideoFunction)
+        self.updateBoxes = form.updateBoxes
+        self.updateBoxes.clicked.connect(self.updateBoxesFunction)        
         self.showLibButton = form.saveToLib
         self.showLibButton.clicked.connect(self.saveToLibFunction)
         self.detTable = form.detailsTable
         
         app.exec_()
+    
+    def updateBoxesFunction(self):
+        self.image = cv2.imread(self.loadedImage)
+        for i in range(1,self.rows):            
+            label = (self.detTable.item(i,0).text())
+            x = int(self.detTable.item(i,1).text())
+            y = int(self.detTable.item(i,2).text())
+            w = int(self.detTable.item(i,3).text())
+            h = int(self.detTable.item(i,4).text())
+            self.draw_bounding_box(self.image, label,self.classes_ids[i-1], x, y, (x+w), (y+h))
+            self.label.setPixmap(QPixmap('object-detection.jpg'))
         
     def quitApp(self):
         QApplication.quit()
@@ -58,10 +74,41 @@ class App(QWidget):
             self.im_counter += 1
             print(self.savedImages)
         
+    def loadVideoFunction(self):
+        self.label.setText('Loading....')  
         
+        desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+        self.loadedVideo = QFileDialog.getOpenFileName(None,'Open file',desktop)[0]
+        
+        self.pixmap = QPixmap(self.loadedVideo)        
+        self.label.setScaledContents(True)
+        self.label.setPixmap(self.pixmap)
+
+        pixmapWidth = 660*0.71
+        pixmapHeight = 660*0.71
+        self.label.resize(pixmapWidth,pixmapHeight)
+        self.label.move(660/2-pixmapWidth/2,660/2-pixmapHeight/2)
+        
+        cap = cv2.VideoCapture(self.loadedVideo)
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret:
+                self.image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                h, w, ch = self.image .shape 
+                self.blob = cv2.dnn.blobFromImage(self.image , self.scale, (416,416), (0,0,0), True, crop=False)
+                self.net.setInput(self.blob)
+                
+                self.yoloPredImage()
+        
+                self.label.setPixmap(QPixmap('object-detection.jpg'))
+            if cv2.waitKey(25) & 0xFF == ord('q'): 
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+          
     def loadImageFunction(self):
         self.label.setText('Loading....')  
-       
+        
         desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
         self.loadedImage = QFileDialog.getOpenFileName(None,'Open file',desktop)[0]
         
@@ -85,12 +132,13 @@ class App(QWidget):
         self.blob = cv2.dnn.blobFromImage(self.image, self.scale, (416,416), (0,0,0), True, crop=False)
         self.net.setInput(self.blob)
         
-        self.yoloPred()
+        self.yoloPredImage()
         
         self.showLibButton.setEnabled(True)
+        self.updateBoxes.setEnabled(True)
         self.label.setPixmap(QPixmap('object-detection.jpg'))
                     
-    def yoloPred(self):
+    def yoloPredImage(self):
         outs = self.net.forward(self.get_output_layers())
         Width = self.image.shape[1]
         Height = self.image.shape[0]        
@@ -132,35 +180,30 @@ class App(QWidget):
             h = box[3]
             ids.append(class_ids[i])
             labels.append(self.classes[class_ids[i]])
-            self.draw_bounding_box(self.image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
-        labels = list(dict.fromkeys(labels))
-        ids = list(dict.fromkeys(ids))
+            self.draw_bounding_box(self.image,str(self.classes[class_ids[i]]), class_ids[i], round(x), round(y), round(x+w), round(y+h))
+        labels = list(labels)
+        ids = list(ids)
         
         self.classes_ids = class_ids
         self.im_indices = indices
-        self.im_boxes = boxes        
-        cv2.imwrite("object-detection.jpg", self.image) 
-        self.fillTable(ids,colors,labels)
+        self.im_boxes = boxes
+        self.fillTable(ids,colors,labels,boxes)        
         
-    def fillTable(self,ids,colors,labels):
-        headers = ["Label", "Red", "Green", "Blue"]
+    def fillTable(self,ids,colors,labels,boxes):
+        headers = ["Label", "X", "Y", "Width","Height"]
         self.col = len(headers)
         self.rows = len(ids) + 1
 
         self.detTable.setRowCount(self.rows)
         self.detTable.setColumnCount(self.col)
-        self.detTable.setItem(0,0, QTableWidgetItem(headers[0]))
-        self.detTable.setItem(0,1, QTableWidgetItem(headers[1]))
-        self.detTable.setItem(0,2, QTableWidgetItem(headers[2]))
-        self.detTable.setItem(0,3, QTableWidgetItem(headers[3]))
-
-        for i in range(0,len(ids)):
-            colors.append(self.COLORS[ids[i]])
         
-        for i in range(1,self.rows):
+        for i in range(0,self.col):
+            self.detTable.setItem(0,i, QTableWidgetItem(headers[i]))
+        
+        for i in range(1,self.rows):            
             self.detTable.setItem(i,0, QTableWidgetItem(labels[i-1]))
             for j in range(1,self.col):
-                self.detTable.setItem(i,j, QTableWidgetItem(str(round(colors[i-1][j-1]))))
+                self.detTable.setItem(i,j, QTableWidgetItem(str(round(boxes[i-1][j-1]))))
             self.detTable.resizeColumnsToContents()       
         
     def get_output_layers(self):
@@ -169,11 +212,11 @@ class App(QWidget):
         return self.output_layers
         
     
-    def draw_bounding_box(self,img, class_id, confidence, x, y, x_plus_w, y_plus_h):
-        self.label_im = str(self.classes[class_id])    
+    def draw_bounding_box(self,img, label, class_id, x, y, x_plus_w, y_plus_h):
         self.color = self.COLORS[class_id]    
         cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), self.color, 2)
-        cv2.putText(img, self.label_im, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, self.color, 2)
+        cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, self.color, 2)
+        cv2.imwrite("object-detection.jpg", self.image)
         
     def center(self):
         qr = self.frameGeometry()
